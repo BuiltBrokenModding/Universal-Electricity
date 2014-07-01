528fe7b97deb69c28156ec35e140b84615b26e98
Robert S <rseifert.phone@gmail.com>
2014-07-01 16:41:57 -0400

    Changed NetworkTickHandler to only dump one network if it crashes.

--- src/main/java/universalelectricity/core/net/NetworkTickHandler.java
+++ src/main/java/universalelectricity/core/net/NetworkTickHandler.java
@@ -9,100 +9,98 @@ import universalelectricity.api.net.IUpdate;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-/**
- * A ticker to update all networks. Register your custom network here to have it ticked by Universal
+/** A ticker to update all networks. Register your custom network here to have it ticked by Universal
  * Electricity.
  * 
- * @author Calclavia
- */
+ * @author Calclavia */
 public class NetworkTickHandler implements ITickHandler
 {
-	public static final NetworkTickHandler INSTANCE = new NetworkTickHandler();
+    public static final NetworkTickHandler INSTANCE = new NetworkTickHandler();
 
-	/** For updaters to be ticked. */
-	private final Set<IUpdate> updaters = Collections.newSetFromMap(new WeakHashMap<IUpdate, Boolean>());
+    /** For updaters to be ticked. */
+    private final Set<IUpdate> updaters = Collections.newSetFromMap(new WeakHashMap<IUpdate, Boolean>());
 
-	/** For queuing Forge events to be invoked the next tick. */
-	private final Queue<Event> queuedEvents = new ConcurrentLinkedQueue<Event>();
+    /** For queuing Forge events to be invoked the next tick. */
+    private final Queue<Event> queuedEvents = new ConcurrentLinkedQueue<Event>();
 
-	public static void addNetwork(IUpdate updater)
-	{
-		synchronized (INSTANCE.updaters)
-		{
-			INSTANCE.updaters.add(updater);
-		}
-	}
+    public static void addNetwork(IUpdate updater)
+    {
+        synchronized (INSTANCE.updaters)
+        {
+            INSTANCE.updaters.add(updater);
+        }
+    }
 
-	public static synchronized void queueEvent(Event event)
-	{
-		synchronized (INSTANCE.queuedEvents)
-		{
-			INSTANCE.queuedEvents.add(event);
-		}
-	}
+    public static synchronized void queueEvent(Event event)
+    {
+        synchronized (INSTANCE.queuedEvents)
+        {
+            INSTANCE.queuedEvents.add(event);
+        }
+    }
 
-	@Override
-	public void tickStart(EnumSet<TickType> type, Object... tickData)
-	{
+    @Override
+    public void tickStart(EnumSet<TickType> type, Object... tickData)
+    {
 
-	}
+    }
 
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData)
-	{
-		/** Tick all updaters. */
-		synchronized (updaters)
-		{
-			Set<IUpdate> removeUpdaters = Collections.newSetFromMap(new WeakHashMap<IUpdate, Boolean>());
+    @Override
+    public void tickEnd(EnumSet<TickType> type, Object... tickData)
+    {
+        /** Tick all updaters. */
+        synchronized (updaters)
+        {
+            Set<IUpdate> removeUpdaters = Collections.newSetFromMap(new WeakHashMap<IUpdate, Boolean>());
 
-			Iterator<IUpdate> updaterIt = new HashSet<IUpdate>(updaters).iterator();
+            Iterator<IUpdate> updaterIt = new HashSet<IUpdate>(updaters).iterator();
 
-			try
-			{
-				while (updaterIt.hasNext())
-				{
-					IUpdate updater = updaterIt.next();
+            while (updaterIt.hasNext())
+            {
+                IUpdate updater = updaterIt.next();
+                try
+                { 
+                    if (updater.canUpdate())
+                    {
+                        updater.update();
+                    }
 
-					if (updater.canUpdate())
-					{
-						updater.update();
-					}
+                    if (!updater.continueUpdate())
+                    {
+                        removeUpdaters.add(updater);
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.out.println(getLabel() + ": Failed while ticking updater. This is a bug!");
+                    removeUpdaters.add(updater);
+                    e.printStackTrace();
+                }
+            }
 
-					if (!updater.continueUpdate())
-					{
-						removeUpdaters.add(updater);
-					}
-				}
+            updaters.removeAll(removeUpdaters);
 
-				updaters.removeAll(removeUpdaters);
-			}
-			catch (Exception e)
-			{
-				System.out.println(getLabel() + ": Failed while tcking updater. This is a bug! Clearing all tickers for self repair.");
-				updaters.clear();
-				e.printStackTrace();
-			}
-		}
+        }
 
-		/** Perform all queued events */
-		synchronized (queuedEvents)
-		{
-			while (!queuedEvents.isEmpty())
-			{
-				MinecraftForge.EVENT_BUS.post(queuedEvents.poll());
-			}
-		}
-	}
+        /** Perform all queued events */
+        synchronized (queuedEvents)
+        {
+            while (!queuedEvents.isEmpty())
+            {
+                MinecraftForge.EVENT_BUS.post(queuedEvents.poll());
+            }
+        }
+    }
 
-	@Override
-	public EnumSet<TickType> ticks()
-	{
-		return EnumSet.of(TickType.SERVER);
-	}
+    @Override
+    public EnumSet<TickType> ticks()
+    {
+        return EnumSet.of(TickType.SERVER);
+    }
 
-	@Override
-	public String getLabel()
-	{
-		return "Universal Electricity Ticker";
-	}
+    @Override
+    public String getLabel()
+    {
+        return "Universal Electricity Ticker";
+    }
 }
